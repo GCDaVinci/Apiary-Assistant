7f73be67686f71b3f6b1b011f50e9ce8e8a7f099
Giancarlo Succi <153232705+GCDaVinci@users.noreply.github.com>
2023-12-07 15:00:33 -0800

    Update I2C.py

--- RPi3B_I2C_Read/I2C.py
+++ RPi3B_I2C_Read/I2C.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python
+import smbus
+import time
+# Define the I2C address to listen for
+address = 0x69
 
+# Create an I2C bus object
+bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi Model B
+
+def receive_integer():
+    try:
+        # Read a single byte from the I2C bus
+        data = bus.read_byte(address)
+        data = bus.read_i2c_block_data(address,0,32)
+        received_message = "".join([chr(byte) for byte in data if byte !=0])
+        print("Received Message:", received_message)
+
+        # Print the received integer
+        # print("Received integer:", data)
+
+    except Exception as e:
+        print("Error:", str(e))
+
+# Main loop
+while True:
+    receive_integer()
+    time.sleep(1)
